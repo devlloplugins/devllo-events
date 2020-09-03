b25d05c6ac6bff9b8c8ed97d1c360d9e19ed0b03
Femi <94sam.fem@gmail.com>
2020-09-03 22:49:12 +0200

    Update template-events.php

--- templates/template-events.php
+++ templates/template-events.php
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function() {
     <div class="row justify-content-center">
       <!-- Column -->
       <div class="col-md-8 text-center">
-        <h3 class="my-3">Upcoming Events</h3>
+        <h3 class="my-3">Events</h3>
         <h6 class="subtitle font-weight-normal">You can relay on our amazing features list and also our customer services will be great experience for you without doubt</h6>
       </div>
       <!-- Column -->
@@ -88,14 +88,19 @@ document.addEventListener('DOMContentLoaded', function() {
     </div>
       <!-- Column -->
       <div class="row">
-      <?php foreach( $posts as $post ) {   ?>
+      <?php foreach( $posts as $post ) { 
+         $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
+         $start_month = get_post_meta( $post->ID, '_start_month', true );
+         $start_day = get_post_meta( $post->ID, '_start_day', true );
+
+        ?>
       <div class="col-md-4 on-hover">
         <div class="card border-0 mb-4">
-          <a href="#"><img class="card-img-top" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/blog/blog-home/img3.jpg" alt="wrappixel kit"></a>
-          <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Oct<span class="d-block">23</span></div>
+          <a href="#"><img class="card-img-top" src="<?php echo $featured_img_url; ?>" alt="wrappixel kit"></a>
+          <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute"><?php echo $start_month; ?><span class="d-block"><?php echo $start_day;?></span></div>
           <h5 class="font-weight-medium mt-3"><a href="<?php echo $url; ?>" class="text-decoration-none link"><?php echo $post->post_title; ?></a></h5>
-          <p class="mt-3">Business Park, Opp. Corns Sam Restaurant, New Yoark, US</p>
-          <a href="#" class="text-decoration-none linking text-themecolor mt-2">Learn More</a>
+          <p class="mt-3"><?php echo wp_strip_all_tags( get_the_excerpt(), true ); ?></p>
+          <a href="<?php echo $url; ?>" class="text-decoration-none linking text-themecolor mt-2">Learn More</a>
         </div>
       </div>
       <?php } ?>
